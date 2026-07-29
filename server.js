6eadea1c1470bac2e09120b9dbe3a927a11dcddb
redoamain <redo.arn@gmail.com>
2026-07-29 09:37:03 +0700

    update server port

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const { createServer } = require('http');
-const { parse } = require('url');
-const next = require('next');
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
 
-const app = next({ dev: false });  // Langsung set false untuk production
+const port = process.env.PORT;
+const app = next({ dev: false });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(3020, (err) => {
+  }).listen(port, (err) => {
     if (err) throw err;
-    console.log('> Ready on http://localhost:3020');
+    console.log(`> Ready on http://localhost:${port}`);
   });
 });
